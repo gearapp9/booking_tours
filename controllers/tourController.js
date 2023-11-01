f8447ca461fbd83324e19671d69621c68a02d20c
malek <kadrimalek103@gmail.com>
2023-11-01 19:43:38 +0100

    creating an alias to get top 5 cheap tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,16 @@
 const ApiFeatures = require("../utils/ApiFeatures");
 const Tour = require("./../models/tourModel");
 
+
+//alias middleware to get top 5 cheap tours
+exports.getTopCheapFive = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  next();
+};
+
 exports.getAllTours = async (req, res, next) => {
-    
   const features = new ApiFeatures(Tour.find(), req.query)
     .filter()
     .sort()
@@ -10,9 +18,7 @@ exports.getAllTours = async (req, res, next) => {
     .page();
 
   try {
-
     const tours = await features.query;
-
 
     res.status(200).json({
       status: "success",
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const tourController = require("./../controllers/tourController");
 
 const router = express.Router();
+router
+  .route("/top-5-cheap")
+  .get(tourController.getTopCheapFive, tourController.getAllTours);
 
 router.route("/").get(tourController.getAllTours);
 
