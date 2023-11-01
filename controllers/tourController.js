cb44e49a6aa95214402a48af53bababfd34d5e42
malek <kadrimalek103@gmail.com>
2023-11-01 20:14:17 +0100

    adding CRUD handlers for the tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,6 @@
 const ApiFeatures = require("../utils/ApiFeatures");
 const Tour = require("./../models/tourModel");
 
-
 //alias middleware to get top 5 cheap tours
 exports.getTopCheapFive = (req, res, next) => {
   req.query.limit = "5";
@@ -30,6 +29,74 @@ exports.getAllTours = async (req, res, next) => {
   } catch (error) {
     res.status(404).json({
       stauts: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.getTour = async (req, res, next) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      stauts: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.createTour = async (req, res, next) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      stauts: "success",
+      data: {
+        newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.updateTour = async (req, res, next) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed",
+      message: error,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res, next) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      stauts: "success",
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed",
       message: error,
     });
   }
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -6,6 +6,15 @@ router
   .route("/top-5-cheap")
   .get(tourController.getTopCheapFive, tourController.getAllTours);
 
-router.route("/").get(tourController.getAllTours);
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route("/:id")
+  .get(tourController.getTour)
+  .post(tourController.createTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
