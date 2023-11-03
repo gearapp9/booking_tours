793d986d2466fdb336e39f02e9c14884b4a8e71e
malek <kadrimalek103@gmail.com>
2023-11-03 12:44:05 +0100

    creating a doc , query and aggragation middleware in tourmodel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,91 +1,98 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
-const tourSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "A tour must have a name"],
-      unique: true,
-      trim: true,
-      maxlength: [40, "A tour name must have less or equal then 40 characters"],
-      minlength: [10, "A tour name must have more or equal then 10 characters"],
-      // validate: [validator.isAlpha, 'Tour name must only contain characters']
-    },
-    slug: String,
-    duration: {
-      type: Number,
-      required: [true, "A tour must have a duration"],
-    },
-    maxGroupSize: {
-      type: Number,
-      required: [true, "A tour must have a group size"],
-    },
-    difficulty: {
-      type: String,
-      required: [true, "A tour must have a difficulty"],
-      enum: {
-        values: ["easy", "medium", "difficult"],
-        message: "Difficulty is either: easy, medium, difficult",
-      },
-    },
-    ratingsAverage: {
-      type: Number,
-      default: 4.5,
-      min: [1, "Rating must be above 1.0"],
-      max: [5, "Rating must be below 5.0"],
-    },
-    ratingsQuantity: {
-      type: Number,
-      default: 0,
-    },
-    price: {
-      type: Number,
-      required: [true, "A tour must have a price"],
-    },
-    priceDiscount: {
-      type: Number,
-      validate: {
-        validator: function (val) {
-          // this only points to current doc on NEW document creation
-          return val < this.price;
-        },
-        message: "Discount price ({VALUE}) should be below regular price",
-      },
-    },
-    summary: {
-      type: String,
-      trim: true,
-      required: [true, "A tour must have a description"],
-    },
-    description: {
-      type: String,
-      trim: true,
-    },
-    imageCover: {
-      type: String,
-      required: [true, "A tour must have a cover image"],
-    },
-    images: [String],
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      select: false,
-    },
-    startDates: [Date],
-    secretTour: {
-      type: Boolean,
-      default: false,
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+    trim: true,
+    maxlength: [40, "A tour name must have less or equal then 40 characters"],
+    minlength: [10, "A tour name must have more or equal then 10 characters"],
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
+  },
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+    enum: {
+      values: ["easy", "medium", "difficult"],
+      message: "Difficulty is either: easy, medium, difficult",
     },
   },
-  // {
-  //   toJSON: { virtuals: true },
-  //   toObject: { virtuals: true },
-  // }
-);
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, "Rating must be above 1.0"],
+    max: [5, "Rating must be below 5.0"],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function (val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: "Discount price ({VALUE}) should be below regular price",
+    },
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
+  },
+});
 
+//document middleware run on save and create:transforming name to a slug ex: Name Game => name-game
 tourSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { name: true });
+  next();
+});
+
+//query middleware on all find to only find tours that are not secret
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+//aggregation middleware to also select (match) tours that are not secret
+tourSchema.pre("aggregation", function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
 });
 
