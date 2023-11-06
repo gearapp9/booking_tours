e560f59c72d92685aa5542a8f77cec497f7153fc
malek <kadrimalek103@gmail.com>
2023-11-06 13:02:59 +0100

    add some security majors to the app

--- app.js
+++ app.js
@@ -1,21 +1,57 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const AppError = require("./utils/AppError");
 const handleGlobalError = require("./controllers/errorController");
+
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
 
 if (process.env.DEV_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
 //add body to req
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 //load static files from public folder
 app.use(express.static(`${__dirname}/public`));
-
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
