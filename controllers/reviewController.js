804f9821f2305dbc2694a3739300c5c717fa091b
malek <kadrimalek103@gmail.com>
2023-11-13 11:49:37 +0100

    populating reviews in tours using virtual property

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,7 @@
 const Review = require("./../models/reviewModel");
 const catchAsync = require("./../utils/catchAsync");
 
+
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   const reviews = await Review.find();
 
@@ -11,6 +12,7 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     },
   });
 });
+
 
 exports.createReview = catchAsync(async (req, res, next) => {
   const newReview = await Review.create(req, body);
