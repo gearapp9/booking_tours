6c0f51484c591b384fb9e30174c89a409a40ee58
malek <130837468+gearapp9@users.noreply.github.com>
2024-07-14 21:47:59 +0200

    Update getDataFunction.ts

--- client/src/utils/getDataFunction.ts
+++ client/src/utils/getDataFunction.ts
@@ -1,4 +1,4 @@
 export const getData = async <T>(url: string): Promise<T> => {
-  const response = await fetch(`https://booking-tours-server-n8h1.onrender.com/${url}`);
+  const response = await fetch(`https://booking-tours-server-n8h1.onrender.com${url}`);
   return await response.json();
 };
