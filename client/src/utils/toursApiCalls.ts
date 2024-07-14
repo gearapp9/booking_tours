478c18d795ec10d7109e3930d7c3457a9b31c792
gearapp9 <kadrimalek103@gmail.com>
2024-07-14 17:58:51 +0200

    more fix

--- client/src/utils/toursApiCalls.ts
+++ client/src/utils/toursApiCalls.ts
@@ -4,9 +4,9 @@ import { getData } from "./getDataFunction";
 
 export const getAllTours = async (searchQury:string|null): Promise<JsonData<Tour[]>> => {
 
-  return await getData<JsonData<Tour[]>>(searchQury ? `/api/v1/tours/${searchQury}` : "/api/v1/tours");
+  return await getData<JsonData<Tour[]>>(searchQury ? `https://booking-tours-server.vercel.app//api/v1/tours/${searchQury}` : "https://booking-tours-server.vercel.app//api/v1/tours");
 };
 
 export const getTour = async (slug: string): Promise<JsonData<Tour>> => {
-  return await getData<JsonData<Tour>>(`/api/v1/tours/tour/${slug}`);
+  return await getData<JsonData<Tour>>(`https://booking-tours-server.vercel.app//api/v1/tours/tour/${slug}`);
 };
