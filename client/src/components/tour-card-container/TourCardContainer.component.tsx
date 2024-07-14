1d1729a5ce07e6fc4a8e1d66c05f07d478bd7596
gearapp9 <kadrimalek103@gmail.com>
2024-07-14 18:03:17 +0200

    test

--- client/src/components/tour-card-container/TourCardContainer.component.tsx
+++ client/src/components/tour-card-container/TourCardContainer.component.tsx
@@ -10,7 +10,8 @@ import { Tour } from "../../models/Tour/Tour";
 const TourCardContainer = () => {
   const tours = useSelector(selectAllToursData) as Tour[]  ;
   const loading = useSelector(selectAllToursIsLoading);
-
+  console.log(tours);
+  
   
   
 
